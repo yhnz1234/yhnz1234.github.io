9115ef054d460e9497ccec74e05cac54e1b416d5
yhnz1234 <myhaoisme@qq.com>
2026-03-30 16:14:59 +0800

    Site updated: 2026-03-30 16:14:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/29/hello-world/"];function toRandomPost(){
+var posts=["2026/03/29/hello-world/","2026/03/30/我的第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
